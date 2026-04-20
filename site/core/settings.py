c24d738e00da03862e2e09630f18bd94b250b0e1
Sasha7b9 <Sasha7b9@tut.by>
2026-04-20 19:42:35 +0300

    *

--- site/core/settings.py
+++ site/core/settings.py
@@ -53,14 +53,14 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'core.urls'
 
-CHANNEL_LAYERS = {
-    'default': {
-        'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
-        },
-    },
-}
+#CHANNEL_LAYERS = {
+#    'default': {
+#        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+#        'CONFIG': {
+#            "hosts": [('127.0.0.1', 6379)],
+#        },
+#    },
+#}
 
 TEMPLATES = [
     {
